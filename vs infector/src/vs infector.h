dea191e9f3e3fe1faa5f04761a722d53290deefa
dwx911 <lewismaddoxx@protonmail.com>
2025-06-15 22:12:16 -0700

    Update vs infector.h

--- vs infector/src/vs infector.h
+++ vs infector/src/vs infector.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
 #include <windows.h>
+#include <iostream>
 
 void infectProjects(const std::wstring& rootDir, const std::wstring& cmd);
+std::wstring getCommonRepoDir();
